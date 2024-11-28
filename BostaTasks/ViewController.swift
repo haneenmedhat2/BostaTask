fef8e9481cd9a474890db432d8b8774cce09ab0a
haneenmedhat2 <haneenmedhat26@gmail.com>
2024-11-28 11:31:48 -0800

    add project to repo

---
+++ BostaTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BostaTasks
+//
+//  Created by Haneen Medhat on 28/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
