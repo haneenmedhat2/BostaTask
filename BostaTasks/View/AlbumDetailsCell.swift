d6687f5abea799b2163869d6fc482c4e43348f94
haneenmedhat2 <haneenmedhat26@gmail.com>
2024-11-29 06:54:19 -0800

    implement album detail's UI

---
+++ BostaTasks/View/AlbumDetailsCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumDetailsCell.swift
+//  BostaTasks
+//
+//  Created by Haneen Medhat on 29/11/2024.
+//
+
+import UIKit
+
+class AlbumDetailsCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImage: UIImageView!
+}
---
+++ BostaTasks/View/AlbumDetailsController.swift
@@ -0,0 +1,31 @@
+//
+//  AlbumDetailsController.swift
+//  BostaTasks
+//
+//  Created by Haneen Medhat on 29/11/2024.
+//
+
+import UIKit
+
+class AlbumDetailsController: UIViewController {
+    @IBOutlet weak var search: UISearchBar!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
